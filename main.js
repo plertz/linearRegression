1c555106bf4b4c62187f9cc805c20dc3feff1fb7
Plertz <nickydeboer2005@gmail.com>
2021-10-27 21:57:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const x = [37.8, 39.3, 45.9, 41.3];
+const y = [22.1, 10.4, 18.3, 18.5];
+
+function predict_sales(radio, weight, bias) {
+    return weight * radio + bias;
+}
---
+++ main.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import random
+
+x = [37.8, 39.3, 45.9, 41.3]
+y = [22.1, 10.4, 18.3, 18.5]
+
+def predict_sales(radio, weight, bias):
+    return weight*radio + bias
+
+def cost_function(radio, sales, weight, bias):
+    companies = len(radio)
+    total_error = 0.0
+    for i in range(companies):
+        total_error += (sales[i] - (weight*radio[i] + bias))**2
+    return total_error / companies
+
+def update_weights(radio, sales, weight, bias, learning_rate):
+    weight_deriv = 0
+    bias_deriv = 0
+    companies = len(radio)
+
+    for i in range(companies):
+        # Calculate partial derivatives
+        # -2x(y - (mx + b))
+        weight_deriv += -2*radio[i] * (sales[i] - (weight*radio[i] + bias))
+
+        # -2(y - (mx + b))
+        bias_deriv += -2*(sales[i] - (weight*radio[i] + bias))
+
+    # We subtract because the derivatives point in direction of steepest ascent
+    weight -= (weight_deriv / companies) * learning_rate
+    bias -= (bias_deriv / companies) * learning_rate
+
+    return weight, bias
+
+def train(radio, sales, weight, bias, learning_rate, iters):
+    cost_history = []
+
+    for i in range(iters):
+        weight,bias = update_weights(radio, sales, weight, bias, learning_rate)
+
+        #Calculate cost for auditing purposes
+        cost = cost_function(radio, sales, weight, bias)
+        cost_history.append(cost)
+        
+    return weight, bias, cost_history
+
+weight, bias, cost_history =(train(x, y, 1, 1, 0.0000001, 1000000))
+print(cost_history, weight, bias)
+# plt.scatter(x, y)
+# plt.show()
